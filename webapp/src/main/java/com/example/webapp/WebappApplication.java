2ed8e2186b22b723f85b9ca82b983d20f27888bd
Wasif Ahmad <911wasif@gmail.com>
2025-01-08 22:26:28 +0000

    Added a simple HelloController to handle '/hello' endpoint

--- webapp/src/main/java/com/example/webapp/WebappApplication.java
+++ webapp/src/main/java/com/example/webapp/WebappApplication.java
@@ -2,6 +2,9 @@ package com.example.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class WebappApplication {
@@ -10,4 +13,13 @@ public class WebappApplication {
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
